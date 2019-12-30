f1f20b0c321432617ca21ae3eeb12cd788224581
lesguillemets <lesguillemets@users.noreply.github.com>
2019-12-30 23:38:15 +0900

    f

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 use rand::prelude::*;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 #[derive(Copy, Debug, Clone)]
 pub enum Cell {
     Dead,
@@ -123,3 +127,21 @@ pub const CONWAY: Rule = Rule {
     alive_min: 2,
     alive_max: 3,
 };
+
+pub type PopulationSeries = Vec<u32>;
+
+pub fn read_serieses(fname: &str) -> HashMap<String, PopulationSeries> {
+    let mut map = HashMap::new();
+    let f = File::open(fname).expect("unable to open file");
+    let reader = BufReader::new(f);
+    for line in reader.lines() {
+        let line = line.expect("unable to read line");
+        let mut words = line.split_whitespace();
+        let header = words.next().expect("empty line?").to_owned();
+        let ys: Vec<u32> = words
+            .map(|n| n.parse::<u32>().unwrap_or_else(|_| panic!("{}", n)))
+            .collect();
+        map.entry(header).or_insert(ys);
+    }
+    map
+}
